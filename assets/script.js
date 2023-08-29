5d2be0e3d9d089010a4be9c282396062bf6064f8
maliacho <maliacho21@gmail.com>
2023-08-29 16:08:18 -0700

    attempted to add bootstrap to HTML and CSS files. ended up reverting back to original custom CSS styling. added psuedocode to JS file

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// need to define
+    // timer
+    // score
+    // user initials
+    // 
+
+// user clicks on "start quiz" button
+
+// questions appear
+    // 3-4 possible options appear
+    //user selects an option
+    //move on to next question
+
+// if the answer is right user recieves a point
+
+// if the answer is wrong the user loses a second on the timer
+
+// after all the questions are answered or the timer reaches 0
+    //user inputs their initials 
+    // initials and score saved on scoreboard
