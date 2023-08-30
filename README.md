8a702f35c5ed254b98e6882f33dcd97fe2e83f25
maliacho <maliacho21@gmail.com>
2023-08-29 20:44:22 -0700

    reformated JS file, made minor adjustments to CSS and HTML file. Updated README. Project still not complete.

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-# code-quiz
-Creating a repo for Challenge 4, which is a code quiz project
+# Description
 
-
-
-## User Story
-
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
-
-```
 GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
@@ -25,5 +11,21 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score
-```
+
+## Challenges 
+
+I still need to practice with JavaScript and become more confident with it. I will be returning to this project to finish it, but this is as far as I was able to get today. 
+
+### Screenshot 
+
+
+
+#### Technologies Used
+HTML, CSS and JavaScript. 
+
+##### Link to Deployed Site
+This is unfinished, it will not work according to the acceptance criteria. I will continue to work on it until it runs the way I'd like. 
+
+
+
 
--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,77 @@
 // need to define
-    // timer
-    // score
+
+let timer = document.querySelector(".timer-count");
+let start = document.querySelector(".start-button");
+
+let timeInterval;
+let timeLeft;
 let score = 0;
-    // user initials
-    // user answer selection
-    // questions and answers
-let questions = [
+    
+// questions and answers
+const questions = [
     {
-        prompt: "Which of the following is NOT a primitive type?\n(a) Boolean\n(b) Data\n(c) String\n(d) Number",
-        answer: "b"
+        question: "What is the name of Troy and Abed's morning show?",
+        options: ["Troy and Abed", "Abed and Troy in the Morning", "Troy and Abed in the Morning", "The Morning Show"],
+        answer: "Troy and Abed in the Morning",
     },
     {
-        prompt: "Which of the following keywords is NOT used to define a variable in JavaScript?\n(a) let\n(b) const\n(c) key\n(d) var",
-        answer: "c"
+        questions: "What is Pierce's brother's name?",
+        options: ["Gary", "Cornelius", "Jeffrey", "Gilbert"],
+        answer: "Gilbert",
     },
     {
-        prompt: "What does HTML stand for?\n(a) Hyper Text Markup Language\n(b) Hyper Text Mardown Language\n(c) Hypo Text Mark Lang\n(d) Hippos That May Lounge",
-        answer: "a"
+        questions: "Who is the Spanish teacher in season 1?",
+        options: ["Señor Chang", "Señora Gonzalez", "Señor Pelton", "Señora Diaz"],
+        answer: "Señor Chang",
     },
     {
-        prompt: "What does CSS stand for?\n(a) Cascading Style Shorts\n(b) Cascading Style Sheets\n(c) Crocodiles Swimming Silently\n(d) Cascading Style Sheet",
-        answer: "b"
+        question: "What is the name of the show Troy and Abed like to watch?",
+        options: [""],
+        answer: "Inspector Spacetime",
+    },
+    {
+        question: "What is the name of Shirley's youngest son?",
+        options: ["Jordan Bennet", "Michael Bennet", "Troy Bennet", "Ben Bennet"],
+        answer: "Ben Bennet",
+    },
+    {
+        question: "Who is the dean of Greendale Community College?",
+        options: ["Jeffrey Winger", "Craig Pelton", "Pierce Hawthorne", "Britta Perry"],
+        answer: "Craig Pelton"
     }
-]
+];
+
+
+
 
 // user clicks on "start quiz" button
-    //click event
-startQuiz.addEventListener("click", quizQuestions)
+start.addEventListener("click", startQuiz)
 
-// questions appear
-    // 3-4 possible options appear
-    //user selects an option
-        //click event
-    //move on to next question
-    for (var i = 0; i < questions.length; i++) {
-        let response = window.prompt(questions[i].prompt)
-        if (response == questions[i].answer) {
-            score++;
-            alert("Correct!")
+// timer starts to countdown
+function startTimer () {
+    timeLeft = 75;
+    timeInterval = setInterval(function(){
+        if (timeLeft > 1) {
+            timer.textContent = timeLeft;
+            timeLeft--;
         } else {
-            alert("Sorry, that's wrong.")
+            timeLeft = "";
+            clearInterval(timeInterval);
         }
-    }
+    },1000);
+timer.textContent = "No time Left"
+};
+
+// begin quiz
+function startQuiz () {
+    let displayQuestion = questions[i];
+
+
+
+
+
+};
+
 
 
 // if the answer is right user recieves a point
